964dce23f14e5982b274bde529ae1ef8f69fa8ab
sjung <retsnomis@gmail.com>
2016-12-20 02:06:32 +0700

    add counter/init stream w counter

--- twit-analysis.py
+++ twit-analysis.py
@@ -6,10 +6,20 @@ from tweepy import Stream
 
 
 class listener(StreamListener):
+    # Add counter to code to make stream stop. Cannot add to on_data since always new call.
+    # Add during init instead, so avail. each time on_data() called.
+    def __init__(self, num_tweets_to_grab):
+        self.counter = 0
+        self.num_tweets_to_grab = num_tweets_to_grab
+
     def on_data(self, data):
-        j = json.loads(data)
-        print(j["text"])
-        return True
+        #try/except bc if find unfriendly tweet json object, skip
+        try:
+            j = json.loads(data)
+            print(j["text"])
+            return True
+        except:
+            pass
 
     def on_error(self, status):
         print(status)
@@ -29,3 +39,9 @@ if __name__ == "__main__":
 
     for trend in trends[0]["trends"]:
         print(trend['name'])
+
+    twitter_stream = Stream(auth, twitter_listener(num_tweets_to_grab=5))
+    try:
+        twitter_stream.sample()
+    except Exception as e:
+        print(e.__doc__)
