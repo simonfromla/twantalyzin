34b7e596b5a5d1b341509da1d6a9d62c9b32f2c4
sjung <retsnomis@gmail.com>
2016-12-23 17:36:27 +0700

    sentAnalysis Fresh

--- twit-analysis.py
+++ twit-analysis.py
@@ -17,19 +17,17 @@ langs = {'ar': 'Arabic', 'bg': 'Bulgarian', 'ca': 'Catalan', 'cs': 'Czech', 'da'
 class Listener(StreamListener):
     # Add counter to code to make stream stop. Cannot add to on_data since always new call.
     # Add during init instead, so avail. each time on_data() called.
-    def __init__(self, num_tweets_to_grab, retweet_count):
+    def __init__(self, num_tweets_to_grab):
         self.counter = 0
         self.num_tweets_to_grab = num_tweets_to_grab
-        self.retweet_count = retweet_count
         self.languages = []
-        self.top_languages = []
 
     def on_data(self, data):
         #try/except bc if find unfriendly tweet json object, skip
         try:
             json_data = json.loads(data)
             # if json_data["retweeted"]:
-            #     print(json_data["text"])
+            #     return
 
 
             self.languages.append(langs[json_data["lang"]])
@@ -38,21 +36,17 @@ class Listener(StreamListener):
 
             # Parse the json object for retweet count
             # retweet_count = json_data["retweeted_status"]["retweet_count"]
-            retweet_count = json_data["retweet_count"]
+            # retweet_count = json_data["retweet_count"]
 
 
             # If the count is gt what its been initialized to(8000),
             #print tweet text, count, lang, and save the top lang
-            if retweet_count >= self.retweet_count:
-                print(json_data["text"], retweet_count, langs[json_data["lang"]])
-                self.top_languages.append(langs[json_data["lang"]])
+            print(json_data["text"], langs[json_data["lang"]])
 
             # These happen at function exit
             if self.counter >= self.num_tweets_to_grab:
                 print(self.languages)
-                print(self.top_languages)
                 print(Counter(self.languages))
-                print(Counter(self.top_languages))
                 return False
 
             return True
@@ -63,7 +57,7 @@ class Listener(StreamListener):
         print(status)
 
 class TwitterMain():
-    def __init__(self, num_tweets_to_grab, retweet_count):
+    def __init__(self, num_tweets_to_grab):
             # access token auth
         self.auth = tweepy.OAuthHandler(cons_tok, cons_sec)
         self.auth.set_access_token(app_tok, app_sec)
@@ -77,7 +71,6 @@ class TwitterMain():
         #     sys.exit(-1)
 
         self.num_tweets_to_grab = num_tweets_to_grab
-        self.retweet_count = retweet_count
 
     def get_trends(self):
         trends = self.api.trends_place(23424960)
@@ -97,9 +90,10 @@ class TwitterMain():
 
     def get_data_stream(self):
         # Init the counter by creating instance with specific # tweets to grab
-        data_stream = Stream(self.auth, Listener(num_tweets_to_grab=self.num_tweets_to_grab, retweet_count=self.retweet_count))
+        data_stream = Stream(self.auth, Listener(num_tweets_to_grab=self.num_tweets_to_grab))
         try:
-            data_stream.filter(follow=["BBCBreaking"])
+            # data_stream.filter(follow=["BBCBreaking"])
+            data_stream.filter(track=["NBA"])
             # data_stream.sample()
         except Exception as e:
             print(e.__doc__)
@@ -118,7 +112,6 @@ if __name__ == "__main__":
     num_tweets_to_grab = 100
     # retweet_count = 500
 
-    # analyze = TwitterMain(num_tweets_to_grab, retweet_count)
     analyze = TwitterMain(num_tweets_to_grab)
     # analyze.get_trends()
     analyze.get_data_stream()
