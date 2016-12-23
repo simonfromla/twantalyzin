3046d9790d02835b4a3b1f6a90f9c428348262b1
sjung <retsnomis@gmail.com>
2016-12-23 21:29:06 +0700

    add dump, settings

---
+++ settings.py
@@ -0,0 +1,9 @@
+TRACK_TERMS = ["NBA", "basketball", "lebron", "curry", "warriors", "cavs"]
+CONNECTION_STRING = ""
+CSV_NAME = "twantalyzin.csv"
+TABLE_NAME = "Stream_Results"
+
+try:
+    from local_config import *
+except Exception:
+    pass
